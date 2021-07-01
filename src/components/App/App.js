436454b8d69a6380765b6fceba2253fb4718c0a3
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-01 12:44:50 +0530

    landing page complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "../Nav/Nav";
+import Landing from "../Landing/Landing";
 import "./App.css";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <Landing />
         {/* Navbar Section */}
         {/* Landing Page */}
         {/* Challenge Sectiuon */}
