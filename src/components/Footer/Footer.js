a5ea7b18c8f30ffc601ef496b09bd2e69bce4f39
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-02 15:14:17 +0530

    footer section complete

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <a
+        href="https://ayushdev.com"
+        className="footer-link"
+        targer="_blank"
+        rel="noreferrer"
+      >
+        Visit my website by clicking the link
+      </a>
+    </div>
+  );
+};
+export default Footer;
