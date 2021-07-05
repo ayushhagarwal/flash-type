89dc5d0da7ce070ed5e4fe73b38ed0c124e05364
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-05 13:26:07 +0530

    typing challenge container and challenge results cards completed

--- src/components/TestContainer/TestContainer.js
+++ src/components/TestContainer/TestContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./TestContainer.css";
 import TryAgain from "../TryAgain/TryAgain";
+import TypingChallengeContainer from "../TypingChallengeContainer/TypingChallengeContainer";
 const TestContainer = ({ words, characters, wpm }) => {
   return (
     <div className="test-container">
-      <div className="try-again-cont">
-        <TryAgain words={words} characters={characters} wpm={wpm} />
+      <div data-aos="fade-up" className="typing-challenge-cont">
+        <TypingChallengeContainer
+          words={words}
+          characters={characters}
+          wpm={wpm}
+        />
       </div>
+      {/* <div className="try-again-cont">
+        <TryAgain words={words} characters={characters} wpm={wpm} />
+      </div> */}
     </div>
   );
 };
