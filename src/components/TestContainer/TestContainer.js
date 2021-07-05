cf979e34ce8032d5187da27ac854d616d02f95c1
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-05 13:42:41 +0530

    typing challenge section that is where we actually type is complete

--- src/components/TestContainer/TestContainer.js
+++ src/components/TestContainer/TestContainer.js
@@ -2,19 +2,24 @@ import React from "react";
 import "./TestContainer.css";
 import TryAgain from "../TryAgain/TryAgain";
 import TypingChallengeContainer from "../TypingChallengeContainer/TypingChallengeContainer";
+
 const TestContainer = ({ words, characters, wpm }) => {
+  const timeRemaining = 0;
   return (
     <div className="test-container">
-      <div data-aos="fade-up" className="typing-challenge-cont">
-        <TypingChallengeContainer
-          words={words}
-          characters={characters}
-          wpm={wpm}
-        />
-      </div>
-      {/* <div className="try-again-cont">
-        <TryAgain words={words} characters={characters} wpm={wpm} />
-      </div> */}
+      {timeRemaining > 0 ? (
+        <div data-aos="fade-up" className="typing-challenge-cont">
+          <TypingChallengeContainer
+            words={words}
+            characters={characters}
+            wpm={wpm}
+          />
+        </div>
+      ) : (
+        <div className="try-again-cont">
+          <TryAgain words={words} characters={characters} wpm={wpm} />
+        </div>
+      )}
     </div>
   );
 };
