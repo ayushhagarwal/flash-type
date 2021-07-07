87ff80ed10eba69cb953d69a156789324f6b4505
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-07 11:26:14 +0530

    states completed and passed to all the components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,31 @@ import Landing from "../Landing/Landing";
 import ChallengeSection from "../ChallengeSection/ChallengeSection";
 import Footer from "../Footer/Footer";
 import "./App.css";
+
+const TotalTime = 60;
+
 class App extends React.Component {
+  state = {
+    selectedParagraph: "Hello",
+    timerStarted: false,
+    timeRemaining: TotalTime,
+    words: 0,
+    characters: 0,
+    wpm: 0,
+  };
   render() {
     return (
       <div className="App">
         <Nav />
         <Landing />
-        <ChallengeSection />
+        <ChallengeSection
+          selectedParagraph={this.state.selectedParagraph}
+          words={this.state.words}
+          characters={this.state.characters}
+          wpm={this.state.wpm}
+          timeRemaining={this.state.timeRemaining}
+          timerStarted={this.state.timerStarted}
+        />
         <Footer />
       </div>
     );
