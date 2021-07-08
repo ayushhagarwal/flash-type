0785eed157b9563141c0c071e99013deec84c038
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-08 15:51:36 +0530

    added the api call to fetch paracgraph for test and made each and every character a separate component

--- src/components/ChallengeSection/ChallengeSection.js
+++ src/components/ChallengeSection/ChallengeSection.js
@@ -9,6 +9,7 @@ const ChallengeSection = ({
   wpm,
   timeRemaining,
   timerStarted,
+  testInfo,
 }) => {
   return (
     <div className="challenge-section-container">
@@ -22,6 +23,7 @@ const ChallengeSection = ({
         wpm={wpm}
         timeRemaining={timeRemaining}
         timerStarted={timerStarted}
+        testInfo={testInfo}
       />
     </div>
   );
