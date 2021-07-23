63b17ef066b044c1714b549bae2d4f0d7a22814c
ayushhagarwal <ayushgrwl365@gmail.com>
2021-07-23 11:13:38 +0530

    completed the test letter component

---
+++ src/components/TestLetter/TestLetter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TestLetter.css";
+
+const TestLetter = ({ individualLetterInfo }) => {
+  const statusClassName = {
+    correct: "test-letter-correct",
+    incorrect: "test-letter-incorrect",
+    notAttempted: "test-letter-not-attempted",
+  }[individualLetterInfo.status];
+  return (
+    <span className={`test-letter ${statusClassName}`}>
+      {individualLetterInfo.testLetter}
+    </span>
+  );
+};
+
+export default TestLetter;
